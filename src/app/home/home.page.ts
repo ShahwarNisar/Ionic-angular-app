132680cd437a1a3a9fd4c3bf5aa7ccc894d58ddb
Shahwar Nisar <shahwarnisar@gmail.com>
2020-09-28 12:59:34 +0500

    Fix menu issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,15 @@ import { Component } from "@angular/core";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  readonly icUser = "/assets/images/ic_user.svg";
+  readonly leftArrow = "/assets/images/left-arrow.svg";
+  readonly dashboard = "/assets/images/dashboard.svg";
+  readonly reporting = "/assets/images/news-admin.svg";
+  readonly coach = "/assets/images/document.svg";
+  readonly analytics = "/assets/images/statistics.svg";
+  readonly alert = "/assets/images/alert.svg";
+  readonly settings = "/assets/images/settings.svg";
+  readonly next = "/assets/images/next-down.svg";
   readonly tabs = [
     {
       name: "DashBoard",
